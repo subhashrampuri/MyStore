9ae00550079787098a352acc919cf135188036de
Subhash Rampuri <49077196+subhashrampuri@users.noreply.github.com>
2019-11-14 21:12:25 +0530

    Add files via upload

---
+++ src/app/products.ts
@@ -0,0 +1,17 @@
+export const products = [
+  {
+    name: 'Samsung S10',
+    price: 800,
+    description: 'A large phone with one of the best screens'
+  },
+  {
+    name: 'MI Note 4',
+    price: 699,
+    description: 'A great phone with robust body and features'
+  },
+  {
+    name: 'Nokia 1100',
+    price: 300,
+    description: 'A great phone will never see the build like nokia'
+  }
+];
